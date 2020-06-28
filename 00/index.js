21997319ddf6d22d8ef77413ffc448af51ac9373
Marian Morosac <mmorosac@amazon.com>
2020-06-28 13:34:19 +0300

    my exercise 00

--- 00/index.js
+++ 00/index.js
@@ -21,3 +21,41 @@ const elButton = document.querySelector('#button');
 elButton.addEventListener('click', () => {
   console.log('I am clicked!');
 })
+
+const machine = {
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        FETCH: 'pending'
+      }
+    },
+    pending: {
+      on: {
+        RESOLVE: 'resolved',
+        REJECT: 'rejected'
+      }
+    },
+    resolved: {},
+    rejected: {}
+  }
+}
+
+const transition = (state, event) => {
+  return machine.states[state]?.on?.[event] || state;
+}
+
+let currentState = machine.initial;
+
+const send = event => {
+  const nextState = transition(currentState, event);
+
+  console.log(nextState);
+
+  currentState = nextState;
+}
+
+window.send = send;
+
+send("CLICK");
+// output(transition('pending', 'RESOLVE'))
